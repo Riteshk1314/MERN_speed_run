11a1a846e556cbfcc26ae5d6937fae855d3ad7c5
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-16 15:00:59 +0530

    Create arrowFunctions.js

---
+++ revising js/arrowFunctions.js
@@ -0,0 +1,20 @@
+//working with arrow functions 
+
+
+export default function DoSomething(){
+
+}
+
+
+
+export const DoSomething2= () =>{
+
+}
+
+
+const MyComponent=()=>{
+    return <div></div>
+}
+
+<button onClick ={  ()=> {console.log("hello")}  } > 
+</button>
