09a3603c8f086df18ff2d777749b103a1cc31acc
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-16 21:11:34 +0530

    Update mapfilterReduce.js

---
+++ revising js/mapfilterReduce.js
@@ -0,0 +1,14 @@
+const names=['pedro','jack', 'jessics ', 'pedro', 'pedro']
+
+names.map((name)=> {
+    return <h1>{name+"1 "}</h1> ;}
+    );
+
+
+
+
+names.filter((name)=>{
+    return name !=="pedro"
+}) 
+
+// .reduce()
