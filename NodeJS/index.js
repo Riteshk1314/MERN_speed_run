264e455ccecec6f57debbe0e77a0abc8ab4ac982
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-29 04:35:30 +0530

    fucking with node

---
+++ NodeJS/index.js
@@ -0,0 +1,5 @@
+console.log("this is nodejs tutorial for beginners")
+console.log("this is the first tutorial on nodejs")
+console.log("this is cove node js with command line")
+// process.exit(0);
+process.exitCode=1
